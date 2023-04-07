5e725e5443bc5b346ea6b9dfbeadbb5e33dc5d90
RogerSellin <sellinroger@gmail.com>
2023-04-07 16:00:59 +0200

    Add: dummy autocomplete

--- shell-complete.sh
+++ shell-complete.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# This script is used to complete the shell command
+
+
+# Dummy autocomplete function
+autocomplete_command() {
+    local command_so_far=$1
+    local possible_completion="ls -l"  # TODO: replace with real completion
+    printf "$possible_completion"
+}
