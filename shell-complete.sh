8aac81ca45cc3f726f0ed64f5cc6963802017888
RogerSellin <sellinroger@gmail.com>
2023-04-08 05:16:55 +0200

    Add: function to record history

--- shell-complete.sh
+++ shell-complete.sh
@@ -4,8 +4,17 @@
 
 
 # Dummy autocomplete function
-autocomplete_command() {
+autocomplete() {
     local command_so_far=$1
     local possible_completion="ls -l"  # TODO: replace with real completion
     printf "$possible_completion"
 }
+
+
+
+record_history() {
+    # this function records the command history with the current directory the command was executed in
+    local current_directory=$PWD
+    local command_so_far=$1
+    echo "$current_directory $command_so_far" >> history.txt
+}    
