4ff177a4a37d49a109eeeaec458c0fc14573aec1
RogerSellin <sellinroger@gmail.com>
2023-09-07 12:48:36 +0200

    Update

--- ai_autocomplete.py
+++ ai_autocomplete.py
@@ -1,29 +1,40 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, LlamaForCausalLM, LlamaTokenizer
+import torch
+import sys
+
+
 
 # Load pre-trained model and tokenizer
 model_name = "gpt2"  
-model = AutoModelForCausalLM.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+if model_name == "gpt2":
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # Create the autocompletion pipeline
-autocompletion_pipeline = pipeline(
-    "text-generation",
-    model=model,
-    tokenizer=tokenizer,
-    config={"max_length": 50}  
-)
-prompt ="This is a linux terminal. There are the following files in the current  directory: Path: , Autocomplete the following linux terminal command and provide no further explanation for the command:"
-#input_list = ["sudo apt", "Autocomplete: sudo apt ", "sudo apt ins","git i","git ini","Autocomplete the following terminal command: git c","git comm"]
-input_list = ["sudo apt","sudo apt up","sudo apt in","ls","py","pyt","pyth","pytho","git","git i","git in","git ini","git co","git comm"]
-suggested_completions = []
+    autocompletion_pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        config={"max_length": 50}  
+    )
 
-# Generate completions for each input item
-for input_text in input_list:
-    completions = autocompletion_pipeline(prompt+input_text, num_return_sequences=3)
-    suggested_completions.append(completions)
+if model_name == "alpaca":
+    base_model = "/path/to/alpaca"
+    model = LlamaForCausalLM.from_pretrained(base_model)
+    tokenizer = LlamaTokenizer.from_pretrained(base_model)
 
-# Print the suggested completions
-for i, completions in enumerate(suggested_completions):
-    print(f"Completions for input {i+1}:")
-    for j, completion in enumerate(completions):
-        print(f"Suggested completion {j+1}: {completion['generated_text'][len(prompt):]}")
+input_text = sys.argv[1]
+files = sys.argv[2]
+path = sys.argv[3]
+premise ="This is a linux terminal."
+file_prompt = "There are the following files in the current  directory:" + files 
+path_prompt = "Path: " + path
+order = "Autocomplete the following linux terminal command and provide no further explanation for the command:"
+
+prompt = premise + file_prompt + path_prompt + order
+
+
+
+ 
+completion = autocompletion_pipeline(prompt+input_text, num_return_sequences=1)
+print(completion['generated_text'][len(prompt):])
